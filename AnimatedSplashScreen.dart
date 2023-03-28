0f03228aa39d8596c05548975feff6cb643eb415
Armin <123939474+ss-armin@users.noreply.github.com>
2023-03-28 18:07:26 +0330

    Add files via upload

---
+++ AnimatedSplashScreen.dart
@@ -0,0 +1,11 @@
+// dependency
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+// code
+AnimatedSplashScreen(
+  duration: ,
+  splash: , // first Screen
+  nextScreen: ,
+  splashTransition: ,
+  backgroundColor: 
+)
